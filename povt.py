4caf9e09c44586de9cf968dbc4a404f932093ab0
sayakbekov <saakbekovamankeldi@gmail.com>
2022-11-07 21:17:03 +0600

    first commit

---
+++ povt.py
@@ -0,0 +1,72 @@
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+# def lent():
+#     for i in range(len(a)):
+#         if a[i] > 5:
+#             print(a[i])
+# lent()
+
+# s = ('banana','stawberry','apple','orange','limon','ananas')
+
+# def ren():
+#     print(s[0])
+#     print(s[5])
+# ren()
+
+# ls= []
+# def tyn():
+#     for i in range(300):
+#         if i <= 237:
+#             ls.append(i)
+#             print(i)
+# tyn()
+
+
+
+
+# spisok_1 = {'Lamborgini', 17, '4456', 2020, 'Paris', 'USA', 11, 23}
+# spisok_2 = {'Ferrari', 17, 4456, 2021, 'Paris', 'UK', 777, 23}
+
+
+
+
+# def ren():
+
+#     print(spisok_1.intersection(spisok_2))
+    
+
+# ren()
+
+# my= [2,4,6,8,10,1,3,5,7,9,11,13,17]
+
+# def none():
+#     for i in range(len(my)):
+#         if i % 2 == 0:
+#             print(my[i])
+
+# none()
+
+# a = [1,0,-2,4,3,6,6,3,5,8,4,2] 
+
+
+# def fin():
+#     b = 1
+#     while b != len(a):
+#         if a[b] > a[b - 1]:
+#             print(a[b])
+#         b += 1
+
+# fin()
+
+
+# azat
+
+# digits = (113,118,-5,1,135,137,0,142,144,17,154,0,155,2,159,172)
+# for i in digits:
+#        print(i*9)
+#  
+
+
+# numbers = [2,4,7,1,8.4,-3.3,7.1,-2,4,-1,7,-43,8,-3,6,9]
+# for i in numbers:
+#        print(i%2,4,7,1,8.4,-3.3,7.1,-2,4,-1,7,-43,8,-3,6,9)
